2e6383001bb0d75d3dbab883a046c9dca5e94a91
rinaldi <rinalfauzi@gmail.com>
2022-10-20 11:30:24 +0700

    first push

---
+++ src/test/java/rinaldifauzi/spring/core/BelajarSpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package rinaldifauzi.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
