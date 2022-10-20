490136cfb669a2403f35ee100c0aa35fcbd5f4f9
rinaldi <rinalfauzi@gmail.com>
2022-10-20 12:36:47 +0700

    membuat duplicate bean

---
+++ src/main/java/rinaldifauzi/spring/core/DuplicateConfiguration.java
@@ -0,0 +1,18 @@
+package rinaldifauzi.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rinaldifauzi.spring.core.data.Foo;
+
+@Configuration
+public class DuplicateConfiguration {
+    @Bean
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2(){
+        return new Foo();
+    }
+}
