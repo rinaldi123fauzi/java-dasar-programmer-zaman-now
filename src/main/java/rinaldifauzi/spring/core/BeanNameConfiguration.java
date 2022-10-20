7cbc8dca4c901691e5d365ffcd4f7bf4859fffa5
rinaldi <rinalfauzi@gmail.com>
2022-10-20 13:18:31 +0700

    mengubah nama bean

---
+++ src/main/java/rinaldifauzi/spring/core/BeanNameConfiguration.java
@@ -0,0 +1,21 @@
+package rinaldifauzi.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import rinaldifauzi.spring.core.data.Foo;
+
+@Configuration
+public class BeanNameConfiguration {
+
+    @Primary
+    @Bean(name = "fooFirst")
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Bean(name = "fooSecond")
+    public Foo foo2(){
+        return new Foo();
+    }
+}
