f0c275390812dffa5679f0e5c6f8f4ec03d6dfd8
rinaldi <rinalfauzi@gmail.com>
2022-10-21 13:24:01 +0700

    bean scope

---
+++ src/main/java/rinaldifauzi/spring/core/ScopeConfiguration.java
@@ -0,0 +1,18 @@
+package rinaldifauzi.spring.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import rinaldifauzi.spring.core.data.Foo;
+
+@Slf4j
+@Configuration
+public class ScopeConfiguration {
+    @Bean
+    @Scope("prototype")
+    public Foo foo(){
+        log.info("Create new Foo");
+        return new Foo();
+    }
+}
