4365b6a65838263e3081110ea22115230334e516
rinaldi <rinalfauzi@gmail.com>
2022-10-21 12:57:32 +0700

    membuat circular dependencies

---
+++ src/main/java/rinaldifauzi/spring/core/cyclic/CyclicB.java
@@ -0,0 +1,10 @@
+package rinaldifauzi.spring.core.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CyclicB {
+    private CyclicC cyclicC;
+}
