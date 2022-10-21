34dd851e62c685cd332f39b116af1e014bbb3720
rinaldi <rinalfauzi@gmail.com>
2022-10-21 13:07:09 +0700

    membuat Depends On

---
+++ src/main/java/rinaldifauzi/spring/core/DependsOnConfiguration.java
@@ -0,0 +1,28 @@
+package rinaldifauzi.spring.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import rinaldifauzi.spring.core.data.Bar;
+import rinaldifauzi.spring.core.data.Foo;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({
+            "bar"
+    })
+    public Foo foo(){
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
+    }
+}
